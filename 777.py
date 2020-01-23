163d60376f5c2d311056d3a3952e1b7f43598ee0
kishore0079 <47566925+kishore0079@users.noreply.github.com>
2020-01-23 21:50:53 +0530

    Create 777.py

---
+++ 777.py
@@ -0,0 +1,10 @@
+a=int(input(""))
+if a<=12 and a>0:
+  if a==1 or a==3 or a==5 or a==7 or a==8 or a==10 or a==12:
+    print("31")
+  elif a==2:
+    print("28")
+  else:
+    print("30")
+else:
+  print("Error")
