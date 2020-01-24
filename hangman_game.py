7b053173d212724e441a542c3fa2ceb9e9df821b
kishore0079 <47566925+kishore0079@users.noreply.github.com>
2020-01-24 20:26:24 +0530

    Create hangman_game.py

---
+++ hangman_game.py
@@ -0,0 +1,28 @@
+s=str(input())
+l=[]
+k=[]
+for i in s:
+    l.append(i)
+    k.append(i)
+m=[]
+def guess():
+    for y in range(0,len(l)):
+        m.append("_")
+    print(m)
+    i=1
+    while(i<=len(s)):
+        a=str(input())
+        if(a in l):
+            n=l.index(a)
+            l[n]="0"
+            while(m[n]=="_"):
+                m[n]=a
+        print(m)
+        i=i+1
+        
+guess();
+if(m==k):
+    print("win")
+else:
+    print("lost")
+print(l)
